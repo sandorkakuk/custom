cf10b6256bd970e557d4ba9d2e19b288a4dca827
sandorkakuk <53403093+sandorkakuk@users.noreply.github.com>
2023-03-31 12:24:00 +0200

    Add files via upload

--- custom.sh
+++ custom.sh
@@ -15,10 +15,10 @@ sed -ie 's/"us"/"hu"/g' /etc/default/keyboard
 apt remove -y ubiquity ubiquity-casper ubiquity-slideshow-ubuntu ubiquity-ubuntu-artwork simple-scan shotwell gparted thunderbird gnome-mahjongg gnome-font-viewer gnome-startup-applications gnome-software gnome-characters gnome-sudoku gnome-mines gnome-calendar gnome-todo gnome-logs gnome-disk-utility gparted transmission-gtk cheese rhythmbox remmina gedit totem deja-dup aisleriot baobab language-selector-gnome update-manager software-properties-gtk update-manager-core usb-creator-gtk update-manager seahorse
 apt remove libreoffice-* -y	
 #installing packages
-apt install -y dialog curl pipenv git python3-tk python3-pygame language-pack-en language-pack-hu gnome-user-docs-hu language-pack-gnome-hu-base language-pack-gnome-hu python3-matplotlib libxcb-xinerama0-dev dialog xdg-user-dirs-gtk git gnome-control-center  qtwebengine5-dev libwebsockets-dev libqt5webkit5 libqt5multimediawidgets5 libqt5svg5 libqt5script5 libqt5scripttools5 libqt5sql5 libqt5texttospeech5 sudo wireshark
+apt install -y dialog curl pipenv git python3-tk python3-pygame language-pack-en language-pack-hu gnome-user-docs-hu language-pack-gnome-hu-base language-pack-gnome-hu python3-matplotlib libxcb-xinerama0-dev dialog xdg-user-dirs-gtk git gnome-control-center  qtwebengine5-dev libwebsockets-dev libqt5webkit5 libqt5multimediawidgets5 libqt5svg5 libqt5script5 libqt5scripttools5 libqt5sql5 libqt5texttospeech5 sudo wireshark fonts-noto-cjk-extra
 #installling PyCharm
 #https://www.how2shout.com/linux/how-to-install-pycharm-ubuntu-20-04-lts/
-wwget https://download.jetbrains.com/python/pycharm-community-2022.3.2.tar.gz
+wget https://download.jetbrains.com/python/pycharm-community-2022.3.3.tar.gz
 cd /opt
 rmdir * -r
 cd
@@ -39,6 +39,8 @@ Exec=/opt/pycharm-community/bin/pycharm.sh
 Icon=/opt/pycharm-community/bin/pycharm.png
 Terminal=false
 EOF
+cd /opt
+rm pycharm
 #
 #installing Visual Studio Code
 #https://code.visualstudio.com/docs/setup/linux
@@ -58,7 +60,7 @@ EOF
 #
 #instaling Cisco Packet Tracer
 apt install xdg-utils gtk-update-icon-cache libgl1-mesa-glx libpulse0 libnss3 libxss1,libasound2 libxslt1.1 libxkbcommon-x11-0 libxcb-xinerama0-dev libfreetype6 -y
-wget https://archive.org/download/pt81_20220222/CiscoPacketTracer_811_Ubuntu_64bit.deb
+wget https://archive.org/download/cisco-packet-tracer-821-ubuntu-64bit/CiscoPacketTracer_821_Ubuntu_64bit.deb
 #dpkg -i CiscoPacketTracer_811_Ubuntu_64bit.deb
 apt-get --yes --force-yes install ./Cisco*
 #Creating a Packet Tracer desktop
@@ -94,12 +96,11 @@ mv /usr/share/applications/org.gnome.PowerStats.desktop /usr/share/applications/
 mv /usr/share/applications/software-properties-drivers.desktop /usr/share/applications/software-properties-drivers.desktop.hide
 mv /var/lib/snapd/desktop/applications/snap-store_ubuntu-software.desktop /var/lib/snapd/desktop/applications/snap-store_ubuntu-software.desktop.hide
 mv /usr/share/applications/gnome-language-selector.desktop/usr/share/applications/gnome-language-selector.desktop.hide
+mv /usr/share/applications/gnome-language-selector.desktop /usr/share/applications/gnome-language-selector.desktop.hide
+#
 rm /etc/init/tty3.conf && rm /etc/init/tty4.conf && rm /etc/init/tty5.conf && rm /etc/init/tty6.conf
 #
-#remove packages
-rm CiscoPacketTracer_811_Ubuntu_64bit.deb
-if [Py -d "$FILE"]; then
-	rm -f Pycharm $FILE
-fi
-apt autoremove -y
+#removing installation packages
+cd
+rm * -R
 #
